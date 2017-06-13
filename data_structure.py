bebec6ad991bf16d8a6df53c65a0015322bb30dd
rockyfire <rockyfire@126.com>
2017-06-13 21:50:30 +0800

    bubble_sort

--- data_structure.py
+++ data_structure.py
@@ -133,3 +133,30 @@ def insert_sort(origin_list):
 origin_list=[2,4,1,6,3]
 # insert_sort(origin_list)
 # print origin_list
+
+
+# 冒泡排序
+
+def bubble_sort(origin_list):
+    for i in range(len(origin_list),0,-1):
+        for j in range(0,i-1):
+            if origin_list[j] > origin_list[j+1]:
+                origin_list[j],origin_list[j+1] =origin_list[j+1],origin_list[j]
+
+origin_list=[5,4,2,1,9,6]
+bubble_sort(origin_list)
+# print origin_list
+
+
+
+
+
+
+
+
+
+
+
+
+
+
